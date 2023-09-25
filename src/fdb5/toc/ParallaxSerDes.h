4b0c96d8bb7f3e5c274ff1458a8ed8730773da3e
Giorgos Saloustros <gesalous@ics.forth.gr>
2023-09-25 17:54:24 +0300

    [fix] Fix debug message

--- src/fdb5/toc/ParallaxSerDes.h
+++ src/fdb5/toc/ParallaxSerDes.h
@@ -60,159 +60,159 @@ private:
 
 template <std::size_t T>
 void ParallaxSerDes<T>::beginObject(const std::string& str) {
-    SERDES_DEBUG("poutses")
+    SERDES_DEBUG("lala")
 }
 
 template <std::size_t T>
 void ParallaxSerDes<T>::endObject() {
-    SERDES_DEBUG("poutses")
+    SERDES_DEBUG("lala")
 }
 
 template <std::size_t T>
 void ParallaxSerDes<T>::nullObject() {
-    SERDES_DEBUG("poutses")
+    SERDES_DEBUG("lala")
 }
 
 template <std::size_t T>
 std::string ParallaxSerDes<T>::nextObject() {
-    SERDES_DEBUG("poutses")
+    SERDES_DEBUG("lala")
     return "";
 }
 
 template <std::size_t T>
 void ParallaxSerDes<T>::doneObject() {
-    SERDES_DEBUG("poutses")
+    SERDES_DEBUG("lala")
 }
 
 template <std::size_t T>
 void ParallaxSerDes<T>::reset() {
-    SERDES_DEBUG("poutses")
+    SERDES_DEBUG("lala")
 }
 
 template <std::size_t T>
 void ParallaxSerDes<T>::load(std::string& string) {
-    SERDES_DEBUG("poutses")
+    SERDES_DEBUG("lala")
 }
 
 template <std::size_t T>
 void ParallaxSerDes<T>::load(float& a) {
-    SERDES_DEBUG("poutses")
+    SERDES_DEBUG("lala")
 }
 
 template <std::size_t T>
 void ParallaxSerDes<T>::load(double& a) {
-    SERDES_DEBUG("poutses")
+    SERDES_DEBUG("lala")
 }
 
 template <std::size_t T>
 void ParallaxSerDes<T>::load(int& a) {
-    SERDES_DEBUG("poutses")
+    SERDES_DEBUG("lala")
 }
 
 template <std::size_t T>
 void ParallaxSerDes<T>::load(unsigned int& a) {
-    SERDES_DEBUG("poutses")
+    SERDES_DEBUG("lala")
 }
 
 template <std::size_t T>
 void ParallaxSerDes<T>::load(long& a) {
-    SERDES_DEBUG("poutses")
+    SERDES_DEBUG("lala")
 }
 
 template <std::size_t T>
 void ParallaxSerDes<T>::load(unsigned long& a) {
-    SERDES_DEBUG("poutses")
+    SERDES_DEBUG("lala")
 }
 
 template <std::size_t T>
 void ParallaxSerDes<T>::load(long long& a) {
-    SERDES_DEBUG("poutses")
+    SERDES_DEBUG("lala")
 }
 
 template <std::size_t T>
 void ParallaxSerDes<T>::load(unsigned long long& a) {
-    SERDES_DEBUG("poutses")
+    SERDES_DEBUG("lala")
 }
 
 template <std::size_t T>
 void ParallaxSerDes<T>::load(char& str) {
-    SERDES_DEBUG("poutses")
+    SERDES_DEBUG("lala")
 }
 
 template <std::size_t T>
 void ParallaxSerDes<T>::load(unsigned char& str) {
-    SERDES_DEBUG("poutses")
+    SERDES_DEBUG("lala")
 }
 
 template <std::size_t T>
 void ParallaxSerDes<T>::dump(const std::string& str) {
-    SERDES_DEBUG("poutses")
+    SERDES_DEBUG("lala")
 }
 
 template <std::size_t T>
 void ParallaxSerDes<T>::dump(float f) {
-    SERDES_DEBUG("poutses")
+    SERDES_DEBUG("lala")
 }
 
 template <std::size_t T>
 void ParallaxSerDes<T>::dump(double d) {
-    SERDES_DEBUG("poutses")
+    SERDES_DEBUG("lala")
 }
 
 template <std::size_t T>
 void ParallaxSerDes<T>::dump(int a) {
-    SERDES_DEBUG("poutses")
+    SERDES_DEBUG("lala")
 }
 
 template <std::size_t T>
 void ParallaxSerDes<T>::dump(unsigned int a) {
-    SERDES_DEBUG("poutses")
+    SERDES_DEBUG("lala")
 }
 
 template <std::size_t T>
 void ParallaxSerDes<T>::dump(long a) {
-    SERDES_DEBUG("poutses")
+    SERDES_DEBUG("lala")
 }
 
 template <std::size_t T>
 void ParallaxSerDes<T>::dump(unsigned long a) {
-    SERDES_DEBUG("poutses")
+    SERDES_DEBUG("lala")
 }
 
 template <std::size_t T>
 void ParallaxSerDes<T>::dump(long long a) {
-    SERDES_DEBUG("poutses")
+    SERDES_DEBUG("lala")
 }
 
 template <std::size_t T>
 void ParallaxSerDes<T>::dump(unsigned long long a) {
-    SERDES_DEBUG("poutses")
+    SERDES_DEBUG("lala")
 }
 
 template <std::size_t T>
 void ParallaxSerDes<T>::dump(char str) {
-    SERDES_DEBUG("poutses")
+    SERDES_DEBUG("lala")
 }
 
 template <std::size_t T>
 void ParallaxSerDes<T>::dump(unsigned char str) {
-    SERDES_DEBUG("poutses")
+    SERDES_DEBUG("lala")
 }
 
 template <std::size_t T>
 void ParallaxSerDes<T>::push(const std::string& str1, const std::string& str2) {
-    SERDES_DEBUG("poutses")
+    SERDES_DEBUG("lala")
 }
 
 template <std::size_t T>
 std::string ParallaxSerDes<T>::get(const std::string& str1) {
-    SERDES_DEBUG("poutses")
+    SERDES_DEBUG("lala")
     return "";
 }
 
 template <std::size_t T>
 void ParallaxSerDes<T>::pop(const std::string& str) {
-    SERDES_DEBUG("poutses")
+    SERDES_DEBUG("lala")
 }
 
 }  // namespace fdb5
